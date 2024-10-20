a9db2c96ac298df68c509c329f8edf1e92fd30b9
subham2006 <submitra23@gmail.com>
2024-10-19 19:31:05 -0700

    added whiteboard component to navbar for edits

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/connect">Connect</Link></li>
+        <li><Link to="/whiteboard">Whiteboard</Link></li>
       </ul>
     </nav>
   );
